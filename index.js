7304b95c7af06657a11b48388133bd24f67770bc
4iar <4iar@users.noreply.github.com>
2016-07-27 02:43:24 +0100

    Use momentjs instead of chrono-node for date parsing and validation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const strftime = require('strftime')
-const chrono = require('chrono-node')
+const moment = require('moment');
 
 const createJsonResponse = require('./createJsonResponse');
 
@@ -15,20 +15,16 @@ const port = process.env.PORT || 8080;
 app.get('/:date', (request, response) => {
   const dateStr = request.params.date;
 
-  let dateObj;
   let time = {natural: null, unix: null};
-  
-  if (isNaN(dateStr)) {
-    dateObj = chrono.parse(dateStr);
-    if (dateObj.length > 0) {
-      let d = dateObj[0].start.date();
-      d.setHours(0, 0, 0, 0);  // force time to 12 am
 
-      time = createJsonResponse(d);
+  if (isNaN(Number(dateStr))) {
+    const d = moment(dateStr);
+    if (d.isValid()) {
+      time = createJsonResponse(d.toDate());
     }
   } else {
-    dateObj = new Date(parseInt(dateStr) * 1000);
-    time = createJsonResponse(dateObj);
+    const d = moment.unix(Number(dateStr));
+    time = createJsonResponse(d.toDate());
   }
 
   response.json(time);
